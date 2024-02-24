ec63eca8f41d7451cc699100bf30d025064e0456
bajisapp@gmail.com <bajisapp@gmail.com>
2024-02-24 23:23:49 +0800

    changes on portfolio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,8 +9,7 @@ const Home = () => {
   return (
     <div name='home' className='w-full  bg-[#020e1f]'>
         {/* container */}
-
-      
+    
 
 
         <div class="container mx-auto grid grid-cols-1 sm:grid-cols-4" style={{paddingTop:'10%', paddingBottom:'10%'}}> 
@@ -19,7 +18,7 @@ const Home = () => {
       <div>
         <p class="text-blue-600">Hi, my name is</p>
         <h1 class="text-2xl sm:text-2xl font-bold text-[#ccd6f6] animate-typing">Balaji K</h1>
-        <h2 class="text-2xl sm:text-3xl font-bold text-[#515974] animate-typing">I'm a web designer</h2>
+        <h2 class="text-2xl sm:text-3xl font-bold text-[#515974] animate-typing">I'm a web designer.</h2>
         <p class="text-[#8892b0] py-4 max-w-[500px] animate-typing">I have a background in Web and Graphics designing and experience of successfully designing a website's layout, usability, and appearance. Over the last few years, I’ve had the opportunity to work for several companies; this has given me extensive Front-end web development / Graphics Design knowledge and technical skills.</p>
         <div>
           <Link to="works" smooth={true} duration={500} className='text-white border-2 hover:bg-blue-600 hover:border-blue-300 px-4 py-4 my-8 mx-auto flex items-center'>
